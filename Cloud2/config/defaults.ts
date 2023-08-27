5da2d71d5866b7a50ece187da156fee82e783990
Audrey <audreyjezy@gmail.com>
2023-08-27 18:16:03 +0100

    postgres docker fix

--- Cloud2/config/defaults.ts
+++ Cloud2/config/defaults.ts
@@ -1,6 +1,6 @@
 export default {
     port: 3001,
-    dbUri: "postgresql://alex:alex@localhost:5432/newdatabase",
+    dbUri: "postgresql://alex:alex@db:5432/newdatabase",
     saltWorkFactor: 10, //how many times to salt the password 
     accessTokenTtl: "15m",
     refreshTokenTtl: "1y",
--- Cloud2/src/utils/pool.ts
+++ Cloud2/src/utils/pool.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    user: process.env.DB_USER || "alex",
+    user: process.env.DB_USERNAME || "alex",
     password: process.env.DB_PASSWORD || "alex",
-    database: process.env.DB_NAME || "newdatabase",
+    database: process.env.DB_DATABASE || "newdatabase",
     host: process.env.DB_HOST || "localhost",
     port: parseInt(process.env.DB_PORT || "5432", 10),
 });
